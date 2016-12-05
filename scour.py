ace29ad21010d0abd744f355e16147545ded6d18
SamGriffith3 <garfunkelnator@gmail.com>
2016-12-04 23:13:03 -0500

    try it

--- scour.py
+++ scour.py
@@ -13,18 +13,26 @@ def search():
 
     # Scraping the site
     first_file = "C:\\Users\\Sam\\Documents\\first.csv"
-    soup = BeautifulSoup(r, 'html.parser')
-    name_box = soup.find('a', attrs={'class': 'result-title hdrlnk'}) # see below
-    time_box = soup.find('time', attrs={'class': 'result-date'})  # need to find a way to get iterate thru
-    name = name_box.text
-    time = time_box.text
 
-    print(time)
-    print(name)
+    for pg in craigslist:
+        soup = BeautifulSoup(r, 'html.parser')
+        name_box = soup.find('a', attrs={'class': 'result-title hdrlnk'}) # see below
+        time_box = soup.find('time', attrs={'class': 'result-date'})  # need to find a way to get iterate thru
+        name = name_box.text
+        time = time_box.text
+        data = []
+        data.append((name, time))
+        not_dup = []
+        if name != not_dup:
+            not_dup.append(name)
+            with open(first_file, 'a') as csv_file:
+                writer = csv.writer(csv_file)
+                for name, time in data:
+                    writer.writerow([time, name])
+            print(time, name)
+        else:
+            continue
 
-    with open(first_file, 'w') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow([time, name])
 
     print("done")
 
